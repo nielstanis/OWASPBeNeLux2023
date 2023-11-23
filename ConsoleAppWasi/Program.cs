61321eeafdc7113bc54fe4f62cad6405c0866e98
Niels <nielstanis@live.nl>
2023-11-23 14:53:16 +0100

    Initial content

---
+++ ConsoleAppWasi/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Good afternoon OWASP BeNeLux 2023!");
+Console.WriteLine($"Running on: {RuntimeInformation.OSArchitecture}!");
+
+var content = System.IO.File.ReadAllText("/etc/hosts");
+Console.WriteLine(content);
